6a0ad8fce32f8fe3f527b2675ba11b326a7c2c7c
Victor E. Puentes <vepuentes@dnestagency.com>
2020-09-13 15:31:03 -0400

    index view index template

--- catalog/views.py
+++ catalog/views.py
@@ -5,6 +5,12 @@ from catalog.models import Book, Author, BookInstance, Genre
 def index(request):
     """View function for home page of site."""
 
+    # Generate counts for genres and books that contain a particular word (case insensitive)
+    num_genre_word = Genre.objects.filter(
+        name__contains='aventure').count()
+    num_books_word = Book.objects.filter(title__contains='soledad').count()
+
+
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
@@ -21,6 +27,8 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genre_word': num_genre_word,
+        'num_books_word': num_books_word
     }
 
     # Render the HTML template index.html with the data in the context variable
